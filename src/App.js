63860b397cf2fd704b9b75ee46640b00a8422ddb
igorgbr <igorgphotoarte@gmail.com>
2021-02-03 23:16:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppProvider } from './context/AppContext';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+  );
+}
+
+export default App;
